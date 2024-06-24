c7dd56860c0ab3f44221ba504f9388fb0504fabb
balinux <ryojuniyantara@gmail.com>
2024-06-24 19:49:16 +0800

    update; lazyvim and add keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- hotkey pindah ke atas dan ke bawah beberapa baris
+vim.api.nvim_set_keymap("n", "<C-k>", "5k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "5j", { noremap = true, silent = true })
