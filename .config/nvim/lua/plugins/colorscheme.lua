83ecec6525961d833fcf5ce65b8a7d6c9619d572
balinux <ryojuniyantara@gmail.com>
2024-12-05 11:17:22 +0800

    add: theme nordic

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -83,6 +83,14 @@ return {
     end,
     opts = {},
   },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nordic").load()
+    end,
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
