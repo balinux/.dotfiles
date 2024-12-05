7d8608af6135c22b5bef58d2c0fdee9f559302fc
balinux <ryojuniyantara@gmail.com>
2024-12-05 11:09:58 +0800

    add: kaganawa theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -57,6 +57,32 @@ return {
       transparent_background = true,
     },
   },
+  {
+    "sho-87/kanagawa-paper.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa-paper").setup({
+        undercurl = true,
+        transparent = false,
+        gutter = false,
+        dimInactive = true, -- disabled when transparent
+        terminalColors = true,
+        commentStyle = { italic = true },
+        functionStyle = { italic = false },
+        keywordStyle = { italic = false, bold = false },
+        statementStyle = { italic = false, bold = false },
+        typeStyle = { italic = false },
+        colors = { theme = {}, palette = {} }, -- override default palette and theme colors
+        overrides = function() -- override highlight groups
+          return {}
+        end,
+      })
+      -- setup must be called before loading
+      vim.cmd("colorscheme kanagawa-paper")
+    end,
+    opts = {},
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
