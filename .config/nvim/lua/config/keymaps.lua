3fe82730a66ba85dc5952658816fb39b449db8c0
balinux <ryojuniyantara@gmail.com>
2024-12-22 10:03:38 +0800

    update tmux

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,7 @@
 -- hotkey pindah ke atas dan ke bawah beberapa baris
 vim.api.nvim_set_keymap("n", "<C-k>", "5k", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-j>", "5j", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", "<C-k>", "5k", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", "<C-j>", "5j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-j>", "<Esc>ja", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", "<C-k>", "<Esc>ka", { noremap = true, silent = true })
---
+++ .config/nvim/lua/plugins/kanban-board.lua
@@ -0,0 +1,7 @@
+return {
+  "balinux/nvim-kanban-board",
+  lazy = false,
+  config = function()
+    require("kanban")
+  end,
+}
